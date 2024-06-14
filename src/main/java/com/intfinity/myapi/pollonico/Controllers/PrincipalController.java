5d9f683a0858dfcf83576e1075ee95c2eba9698c
Nicolas <nicolas.betancur759@pascualbravo.edu.co>
2024-06-14 12:08:16 -0500

    scene principal
    new scene

---
+++ src/main/java/com/intfinity/myapi/pollonico/Controllers/PrincipalController.java
@@ -0,0 +1,16 @@
+package com.intfinity.myapi.pollonico.Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PrincipalController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}

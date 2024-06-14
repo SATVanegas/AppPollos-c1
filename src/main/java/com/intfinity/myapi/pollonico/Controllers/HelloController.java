a0a22af94d7644b1128b0c228d82594bb019922a
Nicolas <nicolas.betancur759@pascualbravo.edu.co>
2024-06-14 17:14:31 -0500

    add scene principal
    new scene principal

--- src/main/java/com/intfinity/myapi/pollonico/Controllers/HelloController.java
+++ src/main/java/com/intfinity/myapi/pollonico/Controllers/HelloController.java
@@ -1,14 +1,52 @@
 package com.intfinity.myapi.pollonico.Controllers;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+
+public class HelloController extends Application {
+
+    @FXML private Button btnEnter;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize() {
+        btnEnter.setOnAction(event -> openSecondView());
+    }
+
+    public void openSecondView() {
+        try {
+            Stage stage = (Stage) btnEnter.getScene().getWindow();
+            stage.close();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/intfinity/myapi/pollonico/Principal.fxml"));
+            Parent root = loader.load();
+            Stage newStage = new Stage();
+            newStage.setTitle("Principal");
+            newStage.setScene(new Scene(root));
+            newStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/intfinity/myapi/pollonico/hello-view.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Welcome");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/intfinity/myapi/pollonico/Controllers/PrincipalController.java
+++ src/main/java/com/intfinity/myapi/pollonico/Controllers/PrincipalController.java
@@ -1,9 +1,32 @@
 package com.intfinity.myapi.pollonico.Controllers;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class PrincipalController extends Application {
+
+    @FXML
+    private Button btnCustomer;
+    @FXML
+    private Button btnSales;
+    @FXML
+    private Button btnExpenses;
+    @FXML
+    private VBox VBPrincipal;
+    @FXML
+    private ImageView image;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +34,65 @@ public class PrincipalController extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/intfinity/myapi/pollonico/Principal.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Principal");
+            primaryStage.show();
+
+            image.setImage(new Image("../resources/com/intfinity/myapi/Images/rg.jpg"));
+
+            // Bind layoutX and layoutY properties to center the ImageView
+            image.layoutXProperty().bind(scene.widthProperty().subtract(image.fitWidthProperty()).divide(2));
+            image.layoutYProperty().bind(scene.heightProperty().subtract(image.fitHeightProperty()).divide(2));
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+
+        VBPrincipal.setTranslateX(-230);
+
+        // Agregar evento para cuando el mouse entra en el botón
+        btnCustomer.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+            btnCustomer.getStyleClass().add("btnsVbox-hover");
+        });
+
+        // Agregar evento para cuando el mouse sale del botón
+        btnCustomer.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+            btnCustomer.getStyleClass().remove("btnsVbox-hover");
+        });
+
+        btnSales.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+            btnSales.getStyleClass().add("btnsVbox-hover");
+        });
+
+        // Agregar evento para cuando el mouse sale del botón
+        btnSales.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+            btnSales.getStyleClass().remove("btnsVbox-hover");
+        });
+
+        btnExpenses.addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
+            btnExpenses.getStyleClass().add("btnsVbox-hover");
+        });
+
+        // Agregar evento para cuando el mouse sale del botón
+        btnExpenses.addEventFilter(MouseEvent.MOUSE_EXITED, event -> {
+            btnExpenses.getStyleClass().remove("btnsVbox-hover");
+        });
+
+        VBPrincipal.addEventFilter(MouseEvent.MOUSE_MOVED, event -> {
+            if (event.getSceneX() <= 100) {  // Si el mouse está muy cerca del borde izquierdo
+                VBPrincipal.setTranslateX(0);
+            } else if (event.getSceneX() > 200) {  // Si el mouse está lejos del menú
+                VBPrincipal.setTranslateX(-230);
+            }
+        });
 
     }
 }
